122bf3e37e7a0c2b3d22640bf443b4e5aeed4011
OlgaJP <olga.pavlova@jetbrains.com>
2016-08-10 16:22:02 +0200

    changes made after mixed reset

---
+++ docker-dir/webservers/weblogic/weblogic12.2.1/domain/container-scripts/create-wls-domain.py
@@ -0,0 +1,60 @@
+domain_name  = os.environ.get("DOMAIN_NAME", "base_domain")
+admin_port   = int(os.environ.get("ADMIN_PORT", "8001"))
+admin_pass   = os.environ.get("ADMIN_PASSWORD", "admin_password")
+cluster_name = os.environ.get("CLUSTER_NAME", "DockerCluster")
+domain_path  = '/u01/oracle/user_projects/domains/' + domain_name
+
+# Open default domain template
+# ======================
+readTemplate("/u01/oracle/wlserver/common/templates/wls/wls.jar")
+
+# Disable Admin Console
+# --------------------
+# cmo.setConsoleEnabled(false)
+
+# Configure the Administration Server and SSL port.
+# =========================================================
+cd('/Servers/AdminServer')
+set('ListenAddress', '')
+set('ListenPort', admin_port)
+
+# Define the user password for weblogic
+# =====================================
+cd('/Security/' + domain_name + '/User/weblogic')
+cmo.setPassword(admin_pass)
+
+# Write the domain and close the domain template
+# ==============================================
+setOption('OverwriteDomain', 'true')
+setOption('ServerStartMode','dev')
+
+cd('/NMProperties')
+set('ListenAddress','')
+set('ListenPort',5556)
+set('CrashRecoveryEnabled', 'true')
+set('NativeVersionEnabled', 'true')
+set('StartScriptEnabled', 'false')
+set('SecureListener', 'false')
+set('LogLevel', 'FINEST')
+
+# Set the Node Manager user name and password
+cd('/SecurityConfiguration/' + domain_name)
+set('NodeManagerUsername', 'weblogic')
+set('NodeManagerPasswordEncrypted', admin_pass)
+
+# Define a WebLogic Cluster
+# =========================
+cd('/')
+create(cluster_name, 'Cluster')
+
+cd('/Clusters/' + cluster_name)
+cmo.setClusterMessagingMode('unicast')
+
+# Write Domain
+# ============
+writeDomain(domain_path)
+closeTemplate()
+
+# Exit WLST
+# =========
+exit()
